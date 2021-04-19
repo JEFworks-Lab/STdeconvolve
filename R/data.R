db9613247827be3ab73d965ef95838c1464c19d3
Brendan Miller <bmill3r@gmail.com>
2021-04-19 19:01:11 -0400

    updated description and data.R to point to mOB and mPOA and BCL datasets

--- R/data.R
+++ R/data.R
@@ -1,16 +1,26 @@
-# Load individual Rds files into list
+#' Spatial transcriptomics transcriptomics of the mouse olfactory bulb
+#'
+#' @format List where 'counts' is a sparse matrix with columns as voxels and rows as genes and
+#'                    'pos' is a data frame of x and y position values per voxel
+#'
+#' @source \url{https://science.sciencemag.org/content/353/6294/78}
+"mOB"
 
-# absolute path but maybe way to just use relative path when working from repo?
-p <- "/Users/brendan/Desktop/PostDoc/work/STDeconvolve/repos/STdeconvolve/data/"
+#' MERFISH data of the mouse pre-optic region for a female naive animal (FN7)
+#'
+#' @format List where 'mat' is a sparse matrix with columns as cells and rows as genes
+#'                          where expression values have already been normalized by volume
+#'                    'pos' is a data frame of x, y, z position values per cell
+#'                          and brain position as 6 slice indices from anterior to posterior
+#'
+#' @source \url{https://science.sciencemag.org/content/362/6416/eaau5324/}
+"mPOA"
 
-rds_names <- list.files(path = p, pattern = "*.Rds", full.names = FALSE)
-data <- lapply(rds_names, function(f){
-  dat <- readRDS(paste0(p,f))
-  dat
-})
-names(data) <- unlist(strsplit(rds_names, ".Rds"))
-
-
-# alternatively, just load in Rds files as the variables they were originally assigned
-# rds_names <- list.files(path = p, pattern = "*.Rds", full.names = TRUE)
-# lapply(rds_names, load, .GlobalEnv)
+#' Spatial transcriptomics transcriptomics of 4 breast cancer biopsy sections
+#'
+#' @format List where 'counts' is a sparse matrix with columns as voxels and rows as genes and
+#'                    'pos' is a data frame of x and y position values per voxel
+#'                          and slice index for 4 consecutive slices
+#'
+#' @source \url{https://science.sciencemag.org/content/353/6294/78}
+"BCL"
